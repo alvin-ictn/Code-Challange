5c32422e3f89abfec1ed14b312c98df067bfcb59
Alvin Mantovani <alvin.ictn@gmail.com>
2020-09-09 08:48:43 +0700

    re-arrange lines

--- script.js
+++ script.js
@@ -1,47 +1,25 @@
-// const input = document.getElementById('input'),
-//       buttonadd = document.getElementById('add'),
-//       list = document.getElementById('lists'),
-//       buttonsort = document.getElementById('sort');
-
-// let todo = [];
-
-// buttonadd.addEventListener('click', add);
-
-// if(todo.length){
-//     show();
-// }
-
-// function show(){
-//     list.innerHTML = "";
-//     for (let i = 0; i < todo.length; i++) {
-//         item.date = new Date(item.date);
-//         list.innerHTML += `<li>${item.text} | ${item.date}${item.completed}</li>`
-//     }
-// }
-// function add(){
-//     let val = input.value;
-//     todo.push({
-//         text: val,
-//         date: new Date(),
-//         completed: false,
-//     })
-//     show();
-// }
-
 // change all of this to jquery code
 
 const input = $('#input');
       buttonadd = $('#add');
       list = $('#lists');
       buttonsort = $('#sort');
-
+// const input = document.getElementById('input'),
+//       buttonadd = document.getElementById('add'),
+//       list = document.getElementById('lists'),
+//       buttonsort = document.getElementById('sort');
 
 let todo = [];
+// let todo = [];
 
 buttonadd.on('click', add);
+// buttonadd.click('click', add);
+// buttonadd.addEventListener('click', add);
 
 if(todo.length) show();
-
+// if(todo.length){
+//     show();
+// }
 
 function show(){
    let render = "";
@@ -51,6 +29,13 @@ function show(){
     }
     list.html(render)
 }
+// function show(){
+//     list.innerHTML = "";
+//     for (let i = 0; i < todo.length; i++) {
+//         item.date = new Date(item.date);
+//         list.innerHTML += `<li>${item.text} | ${item.date}${item.completed}</li>`
+//     }
+// }
 
 function add(){
     let value = input.val();
@@ -62,3 +47,12 @@ function add(){
     show();
     input.val("")
 }
+// function add(){
+//     let val = input.value;
+//     todo.push({
+//         text: val,
+//         date: new Date(),
+//         completed: false,
+//     })
+//     show();
+// }
