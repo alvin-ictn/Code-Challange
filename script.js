a96d20505426ae6bfeed443bffe0ebd40b6e73ed
Alvin Mantovani <alvin.ictn@gmail.com>
2020-09-10 21:37:12 +0700

    initial OOP todo

--- script.js
+++ script.js
@@ -5,47 +5,117 @@ const input = document.getElementById('input'), // memasukan Element DOM kedalam
 // akhir dari deklarasi variable DOM
 
       data = storage('todo'); // ngambil data dari storage
-      
+
+class Todos {
+    constructor({text,date,completed,id}){
+        this.todo = storage('todo') || [];
+        this.text = text;
+        this.date = new Date();
+        this.completed = false;
+        this.id = id || 0;
+    }
+
+    static show(){
+        console.log
+        list.innerHTML = ""; // set innerHTML DOM list menjadi kosong
+
+        // looping untuk nampilin list
+        // let i = 0 deklarasi start number
+        for (let i = 0; i < todo.length; i++) {
+            todo[0].date = new Date(todo[0].date); // convert date string ke format date
+            // menambahkan semua element dalam backthick (template literal) ke dalam innerHTML sebelumnya
+            list.innerHTML +=
+            `<li>
+                ${todo[i].completed ? '<s>' : ''}${todo[i].text} | ${new Date(todo[0].date)}${todo[i].completed ? '</s>' : ''} 
+                <button onclick="Todos.purge(${i})">🗑️</button>
+                <button onclick="Todos.edit(${i}, event)">✏️</button>
+                <button onclick="completeTask(${i}, event)">✔️</button>
+            </li>`
+        }
+    }
+
+    static edit(index, event){
+        let elem = event.target.parentNode; // event target membaca element apa yang di click, event target parentNode membaca parent dari element yang di klik
+        elem.innerHTML = `<input type="text" onkeypress="Todos.done(${index}, event)">` //mengubah list menjadi sebuah input dengan menambahkan fungsi done dan aktif ketika papan ketik di tekan
+    }
+
+    static done(index, event){
+        if (event.which == 13) { // event which 13 adalah tombol enter
+            todo[index].text = event.target.value; // ini untuk mengisi key text pada  objek todo di index yang dituju
+            storage('todo', todo, true); // menyimpan data pada local storage dengan fungsi storage 
+            Todos.show(); // menampilkan ulang data
+        }
+    }
+
+    static purge(index){
+        console.log(index)
+        console.log(event)
+        console.log(event.target)
+        console.log(event.target.value)
+        todo.splice(index, 1);
+        // todo[index].text = event.target.value; // ini untuk mengisi key text pada  objek todo di index yang dituju
+        storage('todo', todo, true); // menyimpan data pada local storage dengan fungsi storage 
+        Todos.show(); // menampilkan ulang data
+        console.log(todo)
+    }
+
+    set add(value){
+        todo.push({
+            text:value,
+            date: new Date(),
+            completed: false
+        })
+        storage('todo', todo, true);
+        Todos.show();
+    }
+
+   
+
+}
+
+let clasdo = new Todos("");
 let todo = data ? data : []; // deklarasi variable todo, masukan data jika ada data jika data array kosong
 
-buttonadd.addEventListener('click', add);
+buttonadd.addEventListener('click', ()=>{
+    clasdo.add = input.value;
+});
 
-// ini fungsi untuk nambahin data
-function add(){
-  let val = input.value; //ngambil value dari DOM input
-  todo.push({
-      text: val, // data inputan
-      date: new Date(), // data tanggal waktu input
-      completed: false, // data kondisi komplit
-  }) // memasukan data kedalam array dari belakang
-  storage('todo', todo, true); //nambahin data ke storage
-  show();
-}
-// akhir fungsi data
+// // ini fungsi untuk nambahin data
+// function add(){
+//   let val = input.value; //ngambil value dari DOM input
+//   todo.push({
+//       text: val, // data inputan
+//       date: new Date(), // data tanggal waktu input
+//       completed: false, // data kondisi komplit
+//   }) // memasukan data kedalam array dari belakang
+//   storage('todo', todo, true); //nambahin data ke storage
+//   show();
+// }
+// // akhir fungsi data
 
 // ini fungsi untuk ngecek apakah todo punya isi gak
 if(todo.length){
-    show();
+    Todos.show();
 }
 
-// ini fungsi untuk menampilkan kedalam HTML
-function show(){
-  list.innerHTML = ""; // set innerHTML DOM list menjadi kosong
+// // ini fungsi untuk menampilkan kedalam HTML
+// function show(){
+//   list.innerHTML = ""; // set innerHTML DOM list menjadi kosong
 
-  // looping untuk nampilin list
-  // let i = 0 deklarasi start number
-  for (let i = 0; i < todo.length; i++) {
-      todo[0].date = new Date(todo[0].date); // convert date string ke format date
-      // menambahkan semua element dalam backthick (template literal) ke dalam innerHTML sebelumnya
-      list.innerHTML +=
-      `<li>
-          ${todo[i].completed ? '<s>' : ''}${todo[i].text} | ${todo[i].date}${todo[i].completed ? '</s>' : ''} 
-          <button onclick="remove(${i})">🗑️</button>
-          <button onclick="edit(${i}, event)">✏️</button>
-          <button onclick="completeTask(${i}, event)">✔️</button>
-      </li>`
-  }
-}
+//   // looping untuk nampilin list
+//   // let i = 0 deklarasi start number
+//   for (let i = 0; i < todo.length; i++) {
+//       todo[0].date = new Date(todo[0].date); // convert date string ke format date
+//       // menambahkan semua element dalam backthick (template literal) ke dalam innerHTML sebelumnya
+//       list.innerHTML +=
+//       `<li>
+//           ${todo[i].completed ? '<s>' : ''}${todo[i].text} | ${todo[i].date}${todo[i].completed ? '</s>' : ''} 
+//           <button onclick="remove(${i})">🗑️</button>
+//           <button onclick="edit(${i}, event)">✏️</button>
+//           <button onclick="completeTask(${i}, event)">✔️</button>
+//       </li>`
+//   }
+// }
 // akhir dari fungsi menampilkan kedalam HTML
 
 // fungsi edit untuk mengubah list yang di edit menjadi input text
@@ -66,12 +136,12 @@ function done(index, event){
 //akhir dari fungsi done
 
 // fungsi remove untuk menghapus data berdasarkan index dari data tersebut
-function remove(index){
-    console.log('remove index', index); // menampilkan pesan pada console log "remove index X"
-    todo.splice(index, 1); // metode pada array untuk membuang array pada index tertentu sebanyak 1 kali
-    storage('todo', todo, true); // menyimpan data pada local storage dengan fungsi storage
-    show(); // menampilkan ulang data
-}
+// function remove(index){
+//     console.log('remove index', index); // menampilkan pesan pada console log "remove index X"
+//     todo.splice(index, 1); // metode pada array untuk membuang array pada index tertentu sebanyak 1 kali
+//     storage('todo', todo, true); // menyimpan data pada local storage dengan fungsi storage
+//     show(); // menampilkan ulang data
+// }
 // akhir dari fungsi done
 
 function completeTask(index){
